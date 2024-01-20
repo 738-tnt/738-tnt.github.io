be75b4210511bc98be15015570d30123bb4f8541
738-tnt <54177938+738-tnt@users.noreply.github.com>
2024-01-20 14:33:47 +0200

    main

--- main.js
+++ main.js
@@ -4,31 +4,31 @@ var urlToBeParsed = "<<URL_TO_BE_PARSED>>";
 const getIframeSrc = async (url) => {
   try {
     const response = await axios.get(url);
-	const htmlString = response.data
-		
-	var parser = new DOMParser();
-	var doc = parser.parseFromString(htmlString, "text/html");
-	
+    const htmlString = response.data
+
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(htmlString, "text/html");
+
     const iframeSrc = doc.querySelector("iframe");
     return iframeSrc.src
   } catch (error) {
-	  console.log(error)
+    console.log(error)
     return error;
   }
 };
 
 const main = async () => {
-	try{
-	const iframeUrl = await getIframeSrc(urlToBeParsed)
-        return iframeUrl;
-	}
-	catch(e){
-		return e.message
-	}
+  try {
+    const iframeUrl = await getIframeSrc(urlToBeParsed)
+    return iframeUrl;
+  }
+  catch (e) {
+    return e.message
+  }
 }
 
 main()
-        .then(r => {
-                window.AndroidInterface.sendData(r);
-        })
+  .then(r => {
+    window.AndroidInterface.sendData(r);
+  })
 
