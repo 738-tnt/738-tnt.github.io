41821a196529d0946262fefa94b0a228e03d2696
738-tnt <54177938+738-tnt@users.noreply.github.com>
2024-01-20 15:33:43 +0200

    main

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ const urlToBeParsed = "<<URL_TO_BE_PARSED>>";
 
 
 const addAxios = () => {
-  // Create a new script element
-  var script = document.createElement('script');
-  // Set the source URL for the Axios library
-  script.src = 'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js';
-  // Append the script element to the document's head
-  document.head.appendChild(script);
+  return new Promise((resolve, reject) => {
+    var script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js';
+    script.onload = resolve;
+    script.onerror = reject;
+    document.head.appendChild(script);
+  });
 }
 
 
@@ -35,7 +36,7 @@ const getIframeSrc = async () => {
 
 const main = async () => {
   try {
-    addAxios()
+    await addAxios()
     const m = await getIframeSrc()
     return m
   }
