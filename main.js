763070486ac4c6a228388d076fa5fa7e3a394e2b
738-tnt <54177938+738-tnt@users.noreply.github.com>
2024-01-20 15:02:23 +0200

    main

--- main.js
+++ main.js
@@ -1,32 +1,34 @@
 const urlToBeParsed = "<<URL_TO_BE_PARSED>>";
 
 
-const getIframeSrc = async() => {
+const getIframeSrc = async () => {
   try {
-    const r = await axios.get(urlToBeParsed);
+    const r = await axios.get("https://score808.us/site-pages/CHTV31");
     const htmlString = r.data;
 
     const parser = new DOMParser();
     const doc = parser.parseFromString(htmlString, "text/html");
     const iframeSrc = doc.querySelector("iframe").src;
 
-    console.log(iframeSrc)
-
-    return iframeSrc;
-    
+    //console.log(iframeSrc);
+    return iframeSrc
   } catch (error) {
     return null
   }
 }
 
+
+
 const main = async () => {
   try {
-    const iframeUrl = await getIframeSrc(urlToBeParsed)
-    return iframeUrl;
+    const m = await fetchData()
+    return m
   }
   catch (e) {
+    console.log(e)
     return null
   }
+
 }
 
 main()
