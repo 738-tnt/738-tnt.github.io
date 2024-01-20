470fd132e5a78f100a2898442212ab6afaa0ad9a
738-tnt <54177938+738-tnt@users.noreply.github.com>
2024-01-20 15:21:27 +0200

    main

--- main.js
+++ main.js
@@ -10,14 +10,16 @@ script.src = 'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js';
 // Append the script element to the document's head
 document.head.appendChild(script);
 }
+
+
 const getIframeSrc = async () => {
   try {
-    return "htmlString"
     const r = await axios.get("https://score808.us/site-pages/CHTV31");
     const htmlString = r.data;
 
     
     
+    return htmlString
 
     const parser = new DOMParser();
     const doc = parser.parseFromString(htmlString, "text/html");
