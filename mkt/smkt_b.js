ad14d961fc441a62197b8438cd9f96d88f476cb1
738-tnt <54177938+738-tnt@users.noreply.github.com>
2024-11-22 15:19:05 +0300

    Update smkt_b.js

--- mkt/smkt_b.js
+++ mkt/smkt_b.js
@@ -1,2 +1,7 @@
 const deliveryData = {"cacheVersion":"1.2.17","deliveryData":{"deliveryDetailsInfo":{"__typename":"DeliveryDetailsInfo","additionalInfo":"","address":"","addressLine1":"","addressLine2":null,"city":"Vaasa","deliveryDate":"2024-11-27","deliveryMethod":"PICKUP","deliveryTime":"10.00","deliverySlotId":"2024-11-27:2c4513e9-71b1-4331-a810-436286223bed","postalCode":"65320","location":null,"deliverySlotPrice":6.9},"selectedAreaId":"7fe64d66-11c4-46d4-937b-f2ec3bd1afd0"}}
-sessionStorage.setItem("delivery-data", JSON.stringify(deliveryData));
+const deliveryState = {"searchInput":"vaasa","address":{"title":"S-market Gerby noutolokero","street":"Västervikintie 2","postalCode":"65280","city":"Vaasa","location":{"latitude":63.13096,"longitude":21.6029}},"method":"PICKUP"}
+const storeData = {"cacheVersion":"1.2.17","storeData":{"deliveryStore":{"__typename":"DeliveryStore","brand":"s-market","id":"639126291","name":"S-market Gerby","areaId":"3cd711ea-89b6-44ec-8a97-3ec33e709fbf"},"storeId":"639126291","selectedBrand":"s-market"}}
+
+sessionStorage.setItem("delivery-data", JSON.stringify(deliveryData))
+sessionStorage.setItem("delivery-state", JSON.stringify(deliveryState))
+sessionStorage.setItem("store-data", JSON.stringify(storeData))
